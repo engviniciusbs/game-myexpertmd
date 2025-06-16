7669684825b9a6cca8e074ca957c87b8cd9ea486
engviniciusbs <engviniciusbs@gmail.com>
2025-06-16 10:51:30 -0300

    chore: update tailwindcss version, remove postcss configuration, and clean up globals.css and layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
       <body className="antialiased" suppressHydrationWarning={true}>
         <div className={`${inter.variable} ${jetbrainsMono.variable}`}>
           {children}
